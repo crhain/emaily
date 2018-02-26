17a3a56195407691ec520623197be01fbc76124b
Carl Hain <crhain@gmail.com>
2018-02-26 01:30:55 -0600

    split out route files and passport setup into seperate files

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
+
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-});
+//load in passport setup
+require('./services/passport');
 
+//load auth routes and pass app to them
+require('./routes/authRoutes')(app);
 
+//start server
 app.listen(PORT);
